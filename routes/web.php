4bb023f32b2167ca3dc08b2df9a2a76e7a6a2d74
trungvo4 <0306211310@caothang.edu.vn>
2024-06-15 15:09:34 +0700

    .

--- routes/web.php
+++ routes/web.php
@@ -5,20 +5,20 @@ use App\Http\Controllers\AdminController;
 use App\Http\Controllers\HomeController;
 
 // Trang home cho người dùng đã đăng nhập
-Route::get('/home', [HomeController::class, 'home'])->name('home');
+
 
 // Trang home cho người dùng chưa đăng nhập
 Route::get('/', [HomeController::class, 'welcome'])->name('welcome');
 // Admin routes
-//Route::group(['prefix' => 'admin', 'middleware' => 'admin'], function () {
- //   Route::get('/', [AdminController::class, 'index'])->name('admin.dashboard');
-//});
+// Route::group(['prefix' => 'admin', 'middleware' => 'admin'], function () {
+//     Route::get('/', [AdminController::class, 'index'])->name('admin.dashboard');
+// });
 
 // User routes
-Route::group(['middleware' => 'auth'], function () {
+Route::group(['middleware' => ['auth', 'admin']], function () {
     Route::get('/home', [HomeController::class, 'index'])->name('user.home');
+    Route::get('/admin', [AdminController::class, 'admin'])->name('admin.dashboard');
 });
 
 // Auth routes
 Auth::routes();
-
